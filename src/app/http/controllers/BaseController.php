956845ffbbd52ad47f8e332cba750f5dd5cdaa7f
ra3ouol <rasoul.a@mabna.com>
2016-02-24 12:54:03 +0330

    set app name for autoload

---
+++ src/app/http/controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ra3oul
+ * Date: 2/24/16
+ * Time: 12:50 PM
+ */
+final class BaseController
+{
+
+}
---
+++ src/app/http/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace
+/**
+ * Created by PhpStorm.
+ * User: ra3oul
+ * Date: 2/24/16
+ * Time: 12:51 PM
+ */
+class HomeController extends
+{
+
+}

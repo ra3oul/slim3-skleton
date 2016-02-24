0fe4d323d4217e73038fe99cb8c5e55e7a7dd790
Majid Abdolhosseini <majid.a@mabna.com>
2016-02-24 17:08:40 +0330

    database configuration file

---
+++ src/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'port'      => '',
+        'database'  => 'tourism',
+        'username'  => 'root',
+        'password'  => '123456',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+];

54ce5605f29ad95219e9b45d04b069558595667d
ra3ouol <rasoul.a@mabna.com>
2016-02-24 12:27:30 +0330

    add eloquent and remove session

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if (PHP_SAPI == 'cli-server') {
 
 require __DIR__ . '/../vendor/autoload.php';
 
-session_start();
+//session_start();
 
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
